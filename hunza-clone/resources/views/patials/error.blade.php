285d475e91bdcf8b779bc97543cf6cfbdeb40a76
Lockig <minhntt2001hn@gmail.com>
2022-12-13 10:48:39 +0700

    Initial commit

---
+++ hunza-clone/resources/views/patials/error.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--begin::Head-->
+<head>
+    <base href="">
+    <meta charset="utf-8"/>
+    <title>@yield('title','Page notfound - Hunza Explorers')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+    <!--end::Fonts-->
+    <!--begin:CSS-->
+{{--    <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css"/>--}}
+    <!--end:CSS-->
+{{--    <link rel="shortcut icon" href="{{mix('media/logos/favicon.ico')}}"/>--}}
+
+    @yield('css')
+</head>
+<body>
+
+</body>
+
+<script>
+
+
+</script>
